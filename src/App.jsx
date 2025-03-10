77f8c3b512e12f006c5f274017ed077ecab32fe2
ryanfkhrji <ryanfakhroji09@gmail.com>
2025-03-10 20:36:03 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Routes, Route } from "react-router-dom";
 import Navbar from "./components/Layouts/Navbar";
 import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
@@ -11,23 +11,22 @@ import { useState } from "react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-
   return (
-    <Router>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-white text-black-custom`}>
         <Section>
           <Navbar />
-          <Routes>
+          <Routes basename="/ryan-portfolio">
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectPage />} />
+            <Route path="/project" element={<ProjectPage />} />
             <Route path="/contact" element={<ContactPage />} />
           </Routes>
           <Footer />
         </Section>
       </div>
-    </Router>
+    </>
   );
 }
 
